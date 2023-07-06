f61b014db138cd4a4341a8f7648b187d90900c9f
TheFred94 <frederikrlarsen@gmail.com>
2023-07-06 09:18:24 +0200

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,9 @@
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus enim beatae. Nam eaque et excepturi sit fugit quis beatae exercitationem, fuga minima eligendi id quidem adipisci praesentium autem odit!</p>
+    </article>
+</body>
