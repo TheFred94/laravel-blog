2e11ac3fcee2a14e54f317ad335eb1254f61c99c
TheFred94 <frederikrlarsen@gmail.com>
2023-07-06 09:23:19 +0200

    Created three blog posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,4 +6,12 @@
         <h1>My First Post</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus enim beatae. Nam eaque et excepturi sit fugit quis beatae exercitationem, fuga minima eligendi id quidem adipisci praesentium autem odit!</p>
     </article>
+    <article>
+        <h1>My Second Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus enim beatae. Nam eaque et excepturi sit fugit quis beatae exercitationem, fuga minima eligendi id quidem adipisci praesentium autem odit!</p>
+    </article>
+    <article>
+        <h1>My Third Post</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam possimus enim beatae. Nam eaque et excepturi sit fugit quis beatae exercitationem, fuga minima eligendi id quidem adipisci praesentium autem odit!</p>
+    </article>
 </body>
