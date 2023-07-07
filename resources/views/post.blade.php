b1ab5bfd1335856d33169fb59da1b18096065ca0
TheFred94 <frederikrlarsen@gmail.com>
2023-07-07 12:03:26 +0200

    added tailwind to the project

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,19 @@
 <title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/app.css">
+</head>
 
 <body>
     <article>
-        <h1><?= $post->title ?></h1>
+        <h1>{!! $post->title !!}</h1>
 
         <div>
-            <?= $post->body; ?>
-
+            <!-- Using the exclamation points to treat it as html -->
+            {!! $post->body !!}
         </div>
     </article>
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,30 @@
 <title>My Blog</title>
-<link rel="stylesheet" href="app.css">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/app.css">
+</head>
+
 
 <body>
 
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
-                </a>
-            </h1>
+    <!-- Using the @ symbol instead of <?php "content" ?> - Will be compiled into vanilla php  -->
 
-            <div>
-                <?= $post->excerpt; ?>
-            </div>
-        </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+    <article class="mt-12 pt-12">
+        <h1>
+            <a class="" href=" /posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+
+        <div>
+            <!-- Excerpt is not HTMl and can be written like this. It's compiled into vanilla php -->
+            {{ $post->excerpt }}
+        </div>
+    </article>
+
+    @endforeach
 </body>
